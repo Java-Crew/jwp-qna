c11a671731fce908aaa9ca9ea6b63d9c1cb0f1f1
JinYoung Park <basejin0810@naver.com>
2022-01-10 21:07:23 +0900

    feat: 유저 게스트 검증 테스트 작성

---
+++ src/test/java/qna/domain/UserTest.java
@@ -0,0 +1,20 @@
+package qna.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import qna.fixture.UserFixture;
+
+@DisplayName("User 테스트")
+class UserTest {
+
+    @Test
+    @DisplayName("게스트 유저인지 판단한다.")
+    void isGuestUser() {
+        User guestUser = User.GUEST_USER;
+        assertThat(guestUser.isGuestUser()).isTrue();
+        assertThat(UserFixture.JAVAJIGI.isGuestUser()).isFalse();
+    }
+
+}
