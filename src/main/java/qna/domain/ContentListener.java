589603262c511619c2eae2910c5da37c7a88f205
pjy1368 <basejin0810@naver.com>
2022-01-10 10:40:28 +0900

    refactor: 클래스 이름으로 ENUM 객체 만들도록 수정

--- src/main/java/qna/domain/ContentListener.java
+++ src/main/java/qna/domain/ContentListener.java
@@ -1,21 +1,21 @@
 package qna.domain;
 
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.PreUpdate;
 
 import qna.common.util.BeanUtil;
 import qna.repository.DeleteHistoryRepository;
 
+import java.util.Locale;
+
 public class ContentListener {
 
     @PreUpdate
     public void preUpdate(Object o) {
-        System.out.println("호출");
         DeleteHistoryRepository deleteHistoryRepository = BeanUtil.getBean(DeleteHistoryRepository.class);
         Content content = (Content) o;
 
-        DiscriminatorValue annotation = Content.class.getAnnotation(DiscriminatorValue.class);
-        DeleteHistory deleteHistory = new DeleteHistory(ContentType.valueOf(annotation.value()), content.getId(), content.getWriter().getId());
+        ContentType contentType = ContentType.valueOf(content.getClass().getSimpleName().toUpperCase(Locale.ROOT));
+        DeleteHistory deleteHistory = new DeleteHistory(contentType, content.getId(), content.getWriter().getId());
 
         deleteHistoryRepository.save(deleteHistory);
     }
