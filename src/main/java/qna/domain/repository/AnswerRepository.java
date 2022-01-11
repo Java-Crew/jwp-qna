6c5c566f1aabcf35da2a22f6bc88a396d10f0ebd
highright96 <nsw5296@naver.com>
2022-01-11 19:42:23 +0900

    refactor: 임베디드 타입 변경으로 인해 Query Methods 방식을 @Query 방식으로 변경

--- src/main/java/qna/domain/repository/AnswerRepository.java
+++ src/main/java/qna/domain/repository/AnswerRepository.java
@@ -1,13 +1,16 @@
 package qna.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import qna.domain.model.Answer;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    @Query("select a from Answer a where a.question.id = ?1 and a.contents.deleted = false")
     List<Answer> findByQuestionIdAndDeletedFalse(Long questionId);
 
+    @Query("select a from Answer a where a.id = ?1 and a.contents.deleted = false")
     Optional<Answer> findByIdAndDeletedFalse(Long id);
 }
--- src/main/java/qna/domain/repository/QuestionRepository.java
+++ src/main/java/qna/domain/repository/QuestionRepository.java
@@ -1,13 +1,16 @@
 package qna.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import qna.domain.model.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    @Query("select q from Question q where q.contents.deleted = false")
     List<Question> findByDeletedFalse();
 
+    @Query("select q from Question q where q.id = ?1 and q.contents.deleted = false")
     Optional<Question> findByIdAndDeletedFalse(Long id);
 }
